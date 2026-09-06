8ac4029dcec301318ac60046600dce8d0940941c
unknown <chandan.akkaram@gmail.com>
2026-09-06 23:35:54 +0530

    bug

---
+++ 6th Aug 26/Bug_code.py
@@ -0,0 +1,17 @@
+def find_largest_even(numbers):
+    largest = None
+
+    for num in numbers:
+        if num % 2 = 0:
+            if largest == None:
+                largest == num
+            elif num > largest:
+                largest = num
+
+    return largest
+
+data = [7, 12, 5, 20, 3, 18, 25]
+
+result = find_largest_even(data)
+
+print("Largest even number is: " + result)
