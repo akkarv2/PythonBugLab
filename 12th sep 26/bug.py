c7703349361ecef067a84ad4c8fc031e069d1bdc
unknown <chandan.akkaram@gmail.com>
2026-09-16 14:17:20 +0530

    update

---
+++ 12th sep 26/bug.py
@@ -0,0 +1,41 @@
+import json
+
+
+def load_users(filename):
+    file = open(filename, "r")
+    data = json.load(file)
+    return data
+
+
+def find_user(users, username):
+    for user in users:
+        if user["name"].lower() == username:
+            return user
+    return None
+
+
+def calculate_average_score(user):
+    total = 0
+
+    for score in user["scores"]:
+        total += score
+
+    average = total / len(user["score"])
+    return average
+
+
+def main():
+    users = load_users("users.json")
+
+    username = input("Enter username: ")
+
+    user = find_user(users, username)
+
+    print("User found:", user["name"])
+
+    average = calculate_average_score(user)
+
+    print("Average score:", round(average, 2))
+
+
+main()
